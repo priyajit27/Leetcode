494a22800a7d8e8d548bd6cdb4c7d9dc508d3b53
Priyajit Paul <97836046+priyajit27@users.noreply.github.com>
2023-05-18 21:32:50 +0530

    Time: 0 ms (100.00%), Space: 11.9 MB (93.41%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        // using STL
+        // next_permutation(nums.begin(),nums.end());
+        
+        // optimal
+        int index=-1;
+        int n=nums.size();
+        for(int i=n-2;i>=0;i--){
+            if(nums[i+1]>nums[i]){
+                index=i;
+                break;
+            }
+        }
+         for(int i=n-1;i>=index && index!=-1 ;i--){
+            if(nums[index]<nums[i]){
+                swap(nums[i],nums[index]);
+                break;
+            }
+        }
+        reverse(nums.begin()+index+1,nums.end());
     }
 };
