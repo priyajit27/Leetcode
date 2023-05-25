906b0ece8225e479f3bf613b9c834b20bc4592b3
Priyajit Paul <97836046+priyajit27@users.noreply.github.com>
2023-05-25 20:04:36 +0530

    Time: 4 ms (82.79%), Space: 9.1 MB (31.16%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+//     Triangle base case opposite so in tabulaton also opposite
     int rec(vector<vector<int>>& triangle,int i,int j,vector<vector<int>>& dp){
         int n=triangle.size();
         if(i==n-1)
@@ -12,7 +13,21 @@ public:
     }
     int minimumTotal(vector<vector<int>>& triangle){
         int n=triangle.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        return rec(triangle,0,0,dp);
+        // vector<vector<int>>dp(n,vector<int>(n,-1));
+        // return rec(triangle,0,0,dp);
+        
+        vector<vector<int>>dp(n,vector<int>(n,0));
+         for(int j=0;j<n;j++)
+           dp[n-1][j]=triangle[n-1][j];
+        
+        for(int i=n-2;i>=0;i--){
+          for(int j=i;j>=0;j--){
+             int down=triangle[i][j]+ dp[i+1][j];
+             int diagonal=triangle[i][j]+ dp[i+1][j+1];
+             dp[i][j]=min(down,diagonal);
+        }  
+        }
+        
+        return dp[0][0];
     }
 };
