25427258bcb9c8da4e0d18cef2b5a9f6fdfe9aa3
Priyajit Paul <97836046+priyajit27@users.noreply.github.com>
2023-05-27 22:41:51 +0530

    Time: 155 ms (38.87%), Space: 42.3 MB (66.60%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,58 @@
+class DisjointSet{
+    //  vector<int>parent,size;
+     public:
+     vector<int>parent,size;
+     DisjointSet(int n){
+         size.resize(n+1,1);
+         parent.resize(n+1);
+         for(int i=0;i<n;i++){
+            parent[i] = i;
+         }
+     }
+
+     int findUPar(int node){
+        if(parent[node]==node)
+        return node;
+        return parent[node]=findUPar(parent[node]);
+     }
+
+     void unionBySize(int u,int v){
+           int ulp_u=findUPar(u);
+           int ulp_v=findUPar(v);
+           if(ulp_u==ulp_v)return;
+           if(size[ulp_u]<size[ulp_v]){
+               parent[ulp_u]=ulp_v;
+               size[ulp_v]+=size[ulp_u];
+           }
+           else{
+             parent[ulp_v]=ulp_u;
+               size[ulp_u]+=size[ulp_v];
+           }
+
+     }
+};
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& edge) {
+         DisjointSet ds(n);
+           int extra=0;
+           for(auto i:edge){
+               int u=i[0];
+               int v=i[1];
+               if(ds.findUPar(u)==ds.findUPar(v)){
+                   extra++;
+               }
+               else
+               ds.unionBySize(u,v);
+           }
+           int count=0;
+           for(int i=0;i<n;i++){
+               if(ds.parent[i]==i)
+               count++;
+           }
+           int ans=count-1;
+           if(extra>=ans)
+           return ans;
+           return -1;
+    }
+};
