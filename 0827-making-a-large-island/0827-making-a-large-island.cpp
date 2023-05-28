ab0bce1442c31a31ffead7cc45a086bbbf646d3d
Priyajit Paul <97836046+priyajit27@users.noreply.github.com>
2023-05-29 01:40:38 +0530

    Time: 515 ms (53.81%), Space: 105.3 MB (89.46%) - LeetHub

---
+++ 0827-making-a-large-island/0827-making-a-large-island.cpp
@@ -0,0 +1,80 @@
+class DisjointSet{
+    //  vector<int>parent,size;
+     public:
+      vector<int>parent,size;
+     DisjointSet(int n){
+         size.resize(n+1,1);
+         parent.resize(n+1);
+         for(int i=0;i<n;i++){
+            parent[i] = i;
+         }
+     }
+
+     int findUPar(int node){
+        if(parent[node]==node)
+        return node;
+        return parent[node]=findUPar(parent[node]);
+     }
+
+     void unionBySize(int u,int v){
+           int ulp_u=findUPar(u);
+           int ulp_v=findUPar(v);
+           if(ulp_u==ulp_v)return;
+           if(size[ulp_u]<size[ulp_v]){
+               parent[ulp_u]=ulp_v;
+               size[ulp_v]+=size[ulp_u];
+           }
+           else{
+             parent[ulp_v]=ulp_u;
+               size[ulp_u]+=size[ulp_v];
+           }
+
+     }
+};
+
+class Solution {
+public:
+    int largestIsland(vector<vector<int>>& grid) {
+          int n=grid.size();
+        DisjointSet ds(n*n);
+        for(int row=0;row<n;row++){
+            for(int col=0;col<n;col++){
+                  int dr[] = { -1, 0, 1, 0};
+                int dc[] = {0, -1, 0, 1};
+               if(grid[row][col]==0) continue;
+               for(int index=0;index<4;index++){
+                   int newr=dr[index]+row;
+                   int newc=dc[index]+col;
+                   if(newr>=0 && newr<n && newc>=0 && newc<n && grid[newr][newc]==1){
+                       int node=row*n+col;
+                       int adjnode=newr*n+newc;
+                       ds.unionBySize(node,adjnode);
+                   }
+               }
+            }
+        }
+        int mx=0;
+        for(int row=0;row<n;row++){
+            for(int col=0;col<n;col++){
+                  int dr[] = { -1, 0, 1, 0};
+                int dc[] = {0, -1, 0, 1};
+                set<int>components;
+               if(grid[row][col]==1) continue;
+               for(int index=0;index<4;index++){
+                   int newr=dr[index]+row;
+                   int newc=dc[index]+col;
+                   if(newr>=0 && newr<n && newc>=0 && newc<n && grid[newr][newc]==1 )
+                    components.insert(ds.findUPar(newr*n+newc));
+               }
+               int totalsize=0;
+               for(auto i:components){
+                   totalsize+=ds.size[i];
+               }
+               mx=max(mx,totalsize+1);
+            }
+        }
+        for(int cell=0;cell<n*n;cell++)
+        mx=max(mx,ds.size[ds.findUPar(cell)]);
+        return mx;
+    }
+};
