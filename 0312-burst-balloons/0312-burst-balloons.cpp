6f5f0c0b77c0f1989c69a86c8fd8fb776eaba815
Priyajit Paul <97836046+priyajit27@users.noreply.github.com>
2023-05-30 01:15:28 +0530

    Time: 444 ms (64.38%), Space: 10.3 MB (76.33%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -16,7 +16,24 @@ public:
         int n=nums.size();
         nums.push_back(1);
         nums.insert(nums.begin(),1);
-            vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-       return  rec(1,n,nums,dp);
+       //      vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+       // return  rec(1,n,nums,dp);
+        
+//         Tabulation
+         vector<vector<int>>dp(n+2,vector<int>(n+2,0));
+        
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=n;j++){
+                if(i>j)
+                    continue;
+                  int maxi=INT_MIN;
+        for(int k=i;k<=j;k++){
+            int steps=nums[i- 1] * nums[k] * nums[j+ 1]+dp[i][k-1]+dp[k+1][j];
+            maxi=max(maxi,steps);
+        }
+     dp[i][j]=maxi;
+    }
+            }
+        return dp[1][n];
     }
 };
