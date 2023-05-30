39012103f75dd5bf8b9199bcd62718420c62fea4
Priyajit Paul <97836046+priyajit27@users.noreply.github.com>
2023-05-31 01:26:30 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.cpp
+++ Rod Cutting - GFG/rod-cutting.cpp
@@ -26,10 +26,47 @@ class Solution{
         return dp[i][n]=max(taken,nottake);
 }
     int cutRod(int cuts[], int n) {
-             vector<vector<int>> dp(n,vector<int>(n+1,-1));
-          return rec(cuts,n-1,n,dp);
+        //      vector<vector<int>> dp(n,vector<int>(n+1,-1));
+        //   return rec(cuts,n-1,n,dp);
         
         
+          // Tabulation
+        //  vector<vector<int>> dp(n,vector<int>(n+1,0));
+         
+        //  for(int i=0;i<=n;i++)
+        //  dp[0][i]=i* cuts[0];
+        //  for(int i=1;i<n;i++){
+        //      for(int j=0;j<=n;j++){
+        //           int nottake=dp[i-1][j];
+        //       int taken=INT_MIN;
+        //       int rodlength=i+1;
+        //         if(rodlength<=j)
+        //         taken=cuts[i]+dp[i][j-rodlength];
+        //   dp[i][j]=max(taken,nottake);
+        //      }
+        //  }
+        //  return dp[n-1][n];
+        
+        
+           // Space optimised\
+         vector<vector<int>> dp(n,vector<int>(n+1,0));
+         
+           vector<int>prev(n+1,0);
+         vector<int>curr(n+1,0);
+         for(int i=0;i<=n;i++)
+         prev[i]=i* cuts[0];
+         for(int i=1;i<n;i++){
+             for(int j=0;j<=n;j++){
+                  int nottake=prev[j];
+              int taken=INT_MIN;
+              int rodlength=i+1;
+                if(rodlength<=j)
+                taken=cuts[i]+curr[j-rodlength];
+          curr[j]=max(taken,nottake);
+             }
+             prev=curr;
+         }
+         return prev[n];
     }
 };
 
