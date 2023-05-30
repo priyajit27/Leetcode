57347493d59916a30b268ad874cf48a9f96d4ce8
Priyajit Paul <97836046+priyajit27@users.noreply.github.com>
2023-05-31 00:06:27 +0530

    Added solution - LeetHub

--- Partitions with Given Difference - GFG/partitions-with-given-difference.cpp
+++ Partitions with Given Difference - GFG/partitions-with-given-difference.cpp
@@ -39,25 +39,49 @@ class Solution {
         
         
         // Tabulation
-        vector<vector<int>>dp(n,vector<int>(s2/2+1,0));
+        // vector<vector<int>>dp(n,vector<int>(s2/2+1,0));
+        // if(arr[0]==0)
+        // dp[0][0]=2;
+        // else
+        // dp[0][0]=1;
+        
+        // if(arr[0]!=0 && arr[0]<=s2/2)
+        //   dp[0][arr[0]]=1;
+        // for(int i=1;i<n;i++){
+        //     for(int j=0;j<=s2/2;j++){
+        //          int notTaken = dp[i-1][j];
+        //          int taken = 0;
+        //         if(arr[i]<=j)
+        //       taken =dp[i-1][j-arr[i]];
+        
+        //       dp[i][j]= (notTaken + taken)%m;
+        //     }
+        // }
+        // return dp[n-1][s2/2];
+        
+        
+        // Space optimised
+         vector<int>prev(s2/2+1,0);
+         vector<int>curr(s2/2+1,0);
         if(arr[0]==0)
-        dp[0][0]=2;
+        prev[0]=2;
         else
-        dp[0][0]=1;
+        prev[0]=1;
         
         if(arr[0]!=0 && arr[0]<=s2/2)
-           dp[0][arr[0]]=1;
+           prev[arr[0]]=1;
         for(int i=1;i<n;i++){
             for(int j=0;j<=s2/2;j++){
-                 int notTaken = dp[i-1][j];
+                 int notTaken = prev[j];
                  int taken = 0;
                 if(arr[i]<=j)
-              taken =dp[i-1][j-arr[i]];
+              taken =prev[j-arr[i]];
         
-               dp[i][j]= (notTaken + taken)%m;
+               curr[j]= (notTaken + taken)%m;
             }
+            prev=curr;
         }
-        return dp[n-1][s2/2];
+        return prev[s2/2];
     }
 };
 
