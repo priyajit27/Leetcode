379607fb2f6ffc1bbd30811d09794967d3a8a5f6
Priyajit Paul <97836046+priyajit27@users.noreply.github.com>
2023-06-05 22:20:08 +0530

    Time: 19 ms (67.26%), Space: 24.1 MB (78.64%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -11,25 +11,49 @@
  */
 class Solution {
 public:
-      void inorder(TreeNode* root,vector<int>& v){
+//        vector<int>v;
+//       void inorder(TreeNode* root,vector<int>& v){
+//         if(root==NULL)
+//             return ;
+//         if(root->left)
+//        inorder(root->left,v);
+//         v.push_back(root->val);
+//         if(root->right)
+//         inorder(root->right,v);
+//     }
+    
+//      TreeNode* ksmallest(TreeNode* root,int k){
+//         if(root==NULL)
+//             return NULL;
+//         if(root->left)
+//        inorder(root->left,v);
+//         v.push_back(root->val);
+//         if(root->right)
+//         inorder(root->right,v);
+//     }
+    void smallest(TreeNode* root, int& k,int& ans){
         if(root==NULL)
             return ;
-        if(root->left)
-       inorder(root->left,v);
-        v.push_back(root->val);
-        if(root->right)
-        inorder(root->right,v);
+        smallest(root->left,k,ans);
+        k--;
+        if(k==0){
+            ans=root->val;
+            return;
+        }
+        smallest(root->right,k,ans);
     }
     int kthSmallest(TreeNode* root, int k) {
-          vector<int>v;
-        inorder(root,v);
+        //   vector<int>v;
+        // inorder(root,v);
+
+//         Naive
+//         int a=0;
+//         for(int i=0;i<k;i++){
+//             a=v[i];
+//         }
+//         return a;
         
-        int a=0;
-        for(int i=0;i<k;i++){
-            a=v[i];
-        }
-        return a;
-        
+//         2nd approach
         // priority_queue<int>q;
         // for(int i=0;i<v.size();i++)
         //     q.push(v[i]);
@@ -38,6 +62,11 @@ public:
         //     q.pop();
         // }
         // return q.top();
-       
+        
+        
+//         N solution
+        int ans=0;
+         smallest(root,k,ans);
+       return ans;
     }
 };
