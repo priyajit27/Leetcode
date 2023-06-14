7a1a4903b185903211c753cae8b40507b02008ee
Priyajit Paul <97836046+priyajit27@users.noreply.github.com>
2023-06-15 00:21:34 +0530

    Time: 23 ms (52.57%), Space: 19.6 MB (17.08%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        // int count=0;
+        // int maxm=0;
+        // sort(nums.begin(),nums.end());
+        // for(int i=1;i<=nums.size();i++){
+        //     if(a[i]==a[i-1])
+        //         count++;
+        //     else 
+        //         count=0;
+        //     maxm=max(maxm,count);
+        // }
+        // return maxm
+        
+        int count=0;
+        int element=0;
+        for(int i=0;i<nums.size();i++){
+            if(count==0){
+                element=nums[i];
+            }
+            if(element==nums[i]){
+                count++;
+            }
+            else
+                count--;
+        }
+        return element;
+    }
+};
