1fd2e6903e3501f56b115799a93c3dcab4ec7ac5
Priyajit Paul <97836046+priyajit27@users.noreply.github.com>
2023-06-14 13:06:57 +0530

    Added solution - LeetHub

--- Find Nth root of M - GFG/find-nth-root-of-m.cpp
+++ Find Nth root of M - GFG/find-nth-root-of-m.cpp
@@ -5,6 +5,37 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
 	public:
+	// If ans can be in point decimal places
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// double multiply(double number, int n) {
+//     double ans = 1.0;
+//     for(int i = 1;i<=n;i++) {
+//         ans = ans * number;
+//     }
+//     return ans; 
+// }
+
+// void getNthRoot(int n, int m) {
+//     double low = 1;
+//     double high = m;
+//     double eps = 1e-7; 
+    
+//     while((high - low) > eps) {
+//         double mid = (low + high) / 2.0; 
+//         if(multiply(mid, n) < m) {
+//             low = mid; 
+//         }
+//         else {
+//             high = mid; 
+//         }
+//     }
+//     cout <<n<<"th root of "<<m<<" is "<<low<<endl; 
+// }
+
+
+
 	int NthRoot(int n, int m){
 	      int low=1;
 	     int high=m;
