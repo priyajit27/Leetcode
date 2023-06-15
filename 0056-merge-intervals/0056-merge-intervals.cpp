99884eae10adc3ccab024b41be519c5b80835c0e
Priyajit Paul <97836046+priyajit27@users.noreply.github.com>
2023-06-15 23:59:16 +0530

    Time: 41 ms (70.52%), Space: 19.1 MB (52.57%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -4,23 +4,33 @@ public:
         int n=arr.size();
         sort(arr.begin(),arr.end());
         vector<vector<int>>ans;
+//         for(int i=0;i<n;i++){
+//             int start=arr[i][0];
+//             int end=arr[i][1];
+            
+//             if(ans.size() && end<=ans[ans.size()-1][1])
+//                 continue;
+            
+            
+//             for(int j=i+1;j<n;j++){
+//                   if(arr[j][0]<=end){
+//                       end=max(arr[j][1],end);
+//                   }
+//                 else
+//                     break;
+//             }
+//             ans.push_back({start,end});
+//             // ans.push_back(end);
+//         }
+        
+        // 0(n) solution
+            
         for(int i=0;i<n;i++){
-            int start=arr[i][0];
-            int end=arr[i][1];
-            
-            if(ans.size() && end<=ans[ans.size()-1][1])
-                continue;
-            
-            
-            for(int j=i+1;j<n;j++){
-                  if(arr[j][0]<=end){
-                      end=max(arr[j][1],end);
-                  }
-                else
-                    break;
+            if(ans.empty() || arr[i][0]>ans.back()[1]){
+                ans.push_back(arr[i]);
             }
-            ans.push_back({start,end});
-            // ans.push_back(end);
+            else
+            ans.back()[1]=max(ans.back()[1],arr[i][1]);
         }
         return ans;
     }
