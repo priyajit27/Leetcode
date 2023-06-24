d24c4a5ae7a6829802d29a9746c45841ab85ddf4
Priyajit Paul <97836046+priyajit27@users.noreply.github.com>
2023-06-24 19:58:09 +0530

    Time: 3 ms (70.29%), Space: 7.3 MB (92.75%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,30 +1,38 @@
 class Solution {
 public:
-      void rec(int index,set<vector<int>>& ans,int n,vector<int>& arr,vector<int>& path){
-    if(index==n){
-        ans.insert(path);
-        return;
-    }
+      void rec(int index,vector<vector<int>>& ans,int n,vector<int>& nums,vector<int>& path){
+//     if(index==n){
+//         ans.insert(path);
+//         return;
+//     }
     
-    // pick
-        path.push_back(arr[index]);
-    rec(index+1,ans,n,arr,path);
+//     // pick
+//         path.push_back(arr[index]);
+//     rec(index+1,ans,n,arr,path);
     
-    // not pick
-        path.pop_back();
-     rec(index+1,ans,n,arr,path);
+//     // not pick
+//         path.pop_back();
+//      rec(index+1,ans,n,arr,path);
+        ans.push_back(path)  ;
+           for(int i=index;i<nums.size();i++){
+             if(i!=index && nums[i]==nums[i-1])continue;
+             path.push_back(nums[i]);
+               rec(i+1,ans,n,nums,path);
+             path.pop_back();
+         }
+          
 }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         
           sort(nums.begin(),nums.end());
           int N=nums.size();
-          set<vector<int>>ans;
+          vector<vector<int>>ans;
         vector<int>path;
          rec(0,ans,N,nums,path);
-          vector<vector<int>>ans1;
-        for(auto i:ans){
-            ans1.push_back(i);
-        }
-         return ans1;
+        //   vector<vector<int>>ans1;
+        // for(auto i:ans){
+        //     ans1.push_back(i);
+        // }
+         return ans;
     }
 };
