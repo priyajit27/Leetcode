e54cebf7fc72926078a046caa62d5b60fcfd849f
Priyajit Paul <97836046+priyajit27@users.noreply.github.com>
2023-06-25 02:05:45 +0530

    Time: 223 ms (48.66%), Space: 76.1 MB (85.07%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -4,20 +4,53 @@ public:
           stack<int>st;
           int area=0;
         int n=histo.size();
-          
-        for(int i=0;i<=n;i++){
-            while(!st.empty() && (i==n || histo[st.top()]>=histo[i])){
-                int width;
-                int height=histo[st.top()];
+        
+        int leftsmall[n];int rightsmall[n];
+        for(int i=0;i<n;i++){
+            
+            while(!st.empty() && histo[st.top()]>=histo[i]){
                 st.pop();
-                if(st.empty())
-                    width=i;
-                else
-                    width=i-st.top()-1;
-                 area=max(area,height*width);
             }
+            if(st.empty())
+                leftsmall[i]=0;
+            else
+                leftsmall[i]=st.top()+1;
             st.push(i);
         }
+        
+        while(!st.empty())
+            st.pop();
+        
+        for(int i=n-1;i>=0;i--){
+             while(!st.empty() && histo[st.top()]>=histo[i]){
+                 st.pop();
+            }
+             if(st.empty())
+                rightsmall[i]=n-1;
+            else
+                rightsmall[i]=st.top()-1;
+            st.push(i);
+        }
+        
+        for(int i=0;i<n;i++){
+            area=max(area,histo[i]*(rightsmall[i]-leftsmall[i]+1));
+        }
+        
+          
+//         Optimal approach
+        // for(int i=0;i<=n;i++){
+        //     while(!st.empty() && (i==n || histo[st.top()]>=histo[i])){
+        //         int width;
+        //         int height=histo[st.top()];
+        //         st.pop();
+        //         if(st.empty())
+        //             width=i;
+        //         else
+        //             width=i-st.top()-1;
+        //          area=max(area,height*width);
+        //     }
+        //     st.push(i);
+        // }
         return area;
     }
 };
