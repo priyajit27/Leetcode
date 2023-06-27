49a34a8a6a4c12e8832c1bf13a609a14a92f202b
Priyajit Paul <97836046+priyajit27@users.noreply.github.com>
2023-06-27 17:28:51 +0530

    Time: 35 ms (87.11%), Space: 89.1 MB (96.77%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -2,29 +2,33 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
 
-       int n1=nums1.size();
-       int n2=nums2.size();
-        int begin1=0;
-        int end1=n1;
-         if (n2 < n1) return findMedianSortedArrays(nums2, nums1);     //    n1 should be less then n2
-        while(begin1<=end1){
-            int i1=begin1+(end1-begin1)/2;
-            int i2=(n1+n2+1)/2-i1;
-             int min1=(i1==n1)?INT_MAX :nums1[i1];
-             int max1=(i1==0)?INT_MIN :nums1[i1-1];
-             int min2=(i2==n2)?INT_MAX :nums2[i2];
-             int max2=(i2==0)?INT_MIN :nums2[i2-1];
-            if(max1<=min2 && max2<=min1){
-                if((n1+n2)%2==0){
-                    return ((double) max(max1,max2) +min(min1,min2))/2.0;
-                }
-                else{
-                    return(double)max(max1,max2);
-                }
+      int n1=nums1.size();
+    int n2=nums2.size();
+    if(n2<n1) return findMedianSortedArrays(nums2,nums1);
+    
+    int low=0,high=n1;
+    while(low<=high){
+        int cut1=(high+low)/2;
+        int cut2=(n1+n2+1)/2-cut1;
+        
+        int left1=cut1==0?INT_MIN:nums1[cut1-1];
+        int left2=cut2==0?INT_MIN:nums2[cut2-1];
+        
+        int right1=cut1==n1?INT_MAX:nums1[cut1];
+        int right2=cut2==n2?INT_MAX:nums2[cut2];
+        
+        if(left1<=right2 && left2<=right1){
+            if((n1+n2)%2==0){
+                return (max(left1,left2)+min(right1,right2))/2.0;
+            }else{
+                return max(left1,left2);
             }
-            else if(max1>min2) end1=i1-1;
-            else begin1=i1+1;
+        }else if(left1>right2){
+            high=cut1-1;
+        }else{
+            low=cut1+1;
         }
-        return -1;  
+    }
+    return 0.0;
     }
 };
