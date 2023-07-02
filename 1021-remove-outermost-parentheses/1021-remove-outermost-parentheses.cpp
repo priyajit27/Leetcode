1a5d8917363e8dd241b60deb7f73d3b097bbd10a
Priyajit Paul <97836046+priyajit27@users.noreply.github.com>
2023-07-02 21:25:36 +0530

    Time: 6 ms (42.19%), Space: 6.8 MB (42.19%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -2,19 +2,38 @@ class Solution {
 public:
     string removeOuterParentheses(string s) {
 //         O(N)  O(N)
-        stack<char>st;
+        // stack<char>st;
+        // string res="";
+        // for(int i=0;i<s.length();i++){
+        //     if(st.empty() && s[i]=='(')
+        //         st.push(s[i]);
+        //     else if(s[i]=='('){
+        //         res+=s[i];
+        //         st.push(s[i]);
+        //     }
+        //     else{
+        //          st.pop();
+        //         if(st.size())
+        //         res+=s[i];
+        //     }
+        // }
+        // return res;
+        
+        // without space complexity
+        
         string res="";
+        int balance=0;
+        
         for(int i=0;i<s.length();i++){
-            if(st.empty() && s[i]=='(')
-                st.push(s[i]);
-            else if(s[i]=='('){
-                res+=s[i];
-                st.push(s[i]);
+            if(s[i]=='('){
+                if(balance>0)
+                   res+=s[i] ;
+                    balance++;
             }
             else{
-                 st.pop();
-                if(st.size())
-                res+=s[i];
+               balance--;
+                  if(balance>0)
+                   res+=s[i] ;
             }
         }
         return res;
