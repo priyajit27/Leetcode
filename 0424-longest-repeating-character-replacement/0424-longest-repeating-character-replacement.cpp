7c777ec938d41bc584598085f1fc6c69058daecf
Priyajit Paul <97836046+priyajit27@users.noreply.github.com>
2023-07-06 03:05:29 +0530

    Time: 9 ms (79.57%), Space: 6.9 MB (98.71%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+//         O(N)         Sc almost O(1) as the array is 26 size only
+         vector<int>cnt(26,0);
+        int left = 0, right = 0;
+        int count = 0;
+        int ans= 0;
+        
+        while(right<s.length()){
+            cnt[s[right]-'A']++;
+            count=max(count, cnt[s[right]-'A']);
+            while(right-left+1-count>k){
+                 cnt[s[left]-'A']--;
+                 left++;
+            }
+            ans=max(ans,right-left+1);
+            right++;
+        }
+        return ans;
+    }
+};
