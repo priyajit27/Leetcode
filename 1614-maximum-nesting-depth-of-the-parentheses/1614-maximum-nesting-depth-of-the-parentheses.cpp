209fc4d9a0bf0468cf381e1bfcfe86c4b0691c9d
Priyajit Paul <97836046+priyajit27@users.noreply.github.com>
2023-07-06 00:30:37 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (18.87%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,13 +1,33 @@
 class Solution {
 public:
     int maxDepth(string s) {
+        stack<char>st;
         int count=0;
-        
+        int maxm=0;
         for(int i=0;i<s.length();i++){
-            if(s[i]==')')count++;
-            else if(s[i]=='(' && count>0)count--;
+            
+            if(count>maxm)
+                maxm=count;
+            
+           if(s[i]=='('){
+               count++;
+               st.push(s[i]);
+           }
+            else if(s[i]==')'){
+                count--;
+                st.pop();
+            }
         }
         
-        return count;
+        return maxm;
+        
+//         int count=0;
+        
+//         for(int i=0;i<s.length();i++){
+//             if(s[i]==')')count++;
+//             else if(s[i]=='(' && count>0)count--;
+//         }
+        
+        // return count;
     }
 };
