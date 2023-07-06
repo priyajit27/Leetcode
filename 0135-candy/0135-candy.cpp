0ac266bbafac3223e18d7e699ea755021e518d35
Priyajit Paul <97836046+priyajit27@users.noreply.github.com>
2023-07-06 16:51:12 +0530

    Time: 26 ms (49.57%), Space: 17.7 MB (71.02%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int candy(vector<int>& rate) {
+        int n=rate.size();
+        vector<int>candy(n,1);
+            for(int i=1;i<n;i++){
+                if(rate[i]>rate[i-1])
+                    candy[i]=candy[i-1]+1;
+            }
+        
+           for(int i=n-2;i>=0;i--){
+                if(rate[i]>rate[i+1])
+                    candy[i]=max(candy[i+1]+1,candy[i]);
+            }
+        
+        int ans=0;
+           for(int i=0;i<n;i++){
+                ans+=candy[i];
+            }
+        return ans;
+    }
+};
