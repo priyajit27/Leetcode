cc945a5f5512b17f3ebbc7bd39bdb78d692dbcba
Priyajit Paul <97836046+priyajit27@users.noreply.github.com>
2023-07-16 18:37:04 +0530

    Time: 11 ms (33.43%), Space: 13.7 MB (98.05%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -15,22 +15,38 @@ public:
         // return ans;
         
 //     approach 2     
-        int rightmax=INT_MIN;int leftmax=INT_MIN;
+//         int rightmax=INT_MIN;int leftmax=INT_MIN;
+//          int n=nums.size();
+//         int prod=1;
+//         for(int i=0;i<n;i++){
+//             prod*=nums[i];
+//             leftmax=max(leftmax,prod);
+//             if(prod==0)
+//                 prod=1;
+//         }
+//          prod=1;
+//           for(int i=n-1;i>=0;i--){
+//               prod*=nums[i];
+//             rightmax=max(rightmax,prod);
+//             if(prod==0)
+//                 prod=1;
+//         }
+//         return max(leftmax,rightmax);
+        
+//  approach 3        
          int n=nums.size();
-        int prod=1;
+        int maxm=INT_MIN;
+        int suff=1;
+        int pre=1;
         for(int i=0;i<n;i++){
-            prod*=nums[i];
-            leftmax=max(leftmax,prod);
-            if(prod==0)
-                prod=1;
+            if(suff==0)
+                suff=1;
+            if(pre==0)
+                pre=1;
+            pre*=nums[i];
+            suff*=nums[n-1-i];
+            maxm=max(maxm,max(suff,pre));
         }
-         prod=1;
-          for(int i=n-1;i>=0;i--){
-              prod*=nums[i];
-            rightmax=max(rightmax,prod);
-            if(prod==0)
-                prod=1;
-        }
-        return max(leftmax,rightmax);
+        return maxm;
     }
 };
