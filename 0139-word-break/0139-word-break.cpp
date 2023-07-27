a01265c4ee5bac10599a37df676fabc5de4f0500
Priyajit Paul <97836046+priyajit27@users.noreply.github.com>
2023-07-27 20:46:45 +0530

    Time: 28 ms (24.22%), Space: 14.7 MB (19.17%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool rec(int i,string s,set<string>& sets,vector<string>& wordDict,vector<int>& dp){
+        if(i==s.size())return true;
+        if(dp[i]!=-1)return dp[i];
+        
+        for(int j=i;j<s.size();j++){
+            string temp=s.substr(i,j-i+1);
+            if(sets.find(temp)!=sets.end() && rec(j+1,s,sets,wordDict,dp))
+                return dp[i] =true;
+        }
+        return dp[i]=false;
+    }
+    
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n=s.length();
+        set<string>sets(wordDict.begin(),wordDict.end());
+        vector<int>dp(n+1,-1);
+        return rec(0,s,sets,wordDict,dp);
+    }
+};
