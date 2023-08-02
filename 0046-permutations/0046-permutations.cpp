88441c1107a33e45bf0b5e6ecb968ef5043d86c8
Priyajit Paul <97836046+priyajit27@users.noreply.github.com>
2023-08-02 09:00:31 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (60.28%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,30 @@
+class Solution {
+private:
+  void repermute( vector<int>& ds,vector<int>& nums, vector<vector<int>>& ans,int fre[]){
+      if(ds.size()==nums.size()){
+          ans.push_back(ds);
+        //   return;
+      }
+      for(int i=0;i<nums.size();i++){
+           if(!fre[i]){
+               ds.push_back(nums[i]);
+               fre[i]=1;
+               repermute(ds,nums,ans,fre);
+                fre[i]=0;
+                ds.pop_back();
+           }
+      }
+  }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        vector<int>ds;
+        // int fre[nums.size()]={0};
+        int fre[nums.size()];
+        for(int i=0;i<nums.size();i++){
+            fre[i]=0;
+        }
+        repermute(ds,nums,ans,fre);
+        return ans;
+    }
+};
