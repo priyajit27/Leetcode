b6dc863598ca3b2db1ee305bc997217756f021da
Priyajit Paul <97836046+priyajit27@users.noreply.github.com>
2023-09-09 13:02:08 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (85.45%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -18,7 +18,7 @@ public:
         
         vector<unsigned int>dp(target+1,0);
         dp[0]=1;
-        for(int i=1;i<dp.size();i++){
+        for(int i=1;i<=target;i++){
             for(int j=0;j<nums.size();j++){
                 if(i-nums[j]>=0)
                dp[i]+= dp[i-nums[j]];
