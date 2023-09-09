c59b17a41f16251c0c7db986339cd36b5922a258
Priyajit Paul <97836046+priyajit27@users.noreply.github.com>
2023-09-09 12:27:06 +0530

    Time: 3 ms (50.37%), Space: 6.6 MB (85.45%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int rec(vector<int>& nums,int target, vector<int>& dp){
+        if(target<=0){
+             if(target==0)return 1;
+            return 0;
+        }
+        if(dp[target]!=-1)dp[target];
+        int ans=0;
+       for(int i=0;i<nums.size();i++){
+          ans+= rec(nums,target-nums[i],dp);
+       }
+        return dp[target]=ans;
+    }
+    int combinationSum4(vector<int>& nums,int target) {
+       //  vector<int>dp(target+1,-1);
+       // return rec(nums,target,dp);
+        
+        vector<unsigned int>dp(target+1,0);
+        dp[0]=1;
+        for(int i=1;i<dp.size();i++){
+            for(int j=0;j<nums.size();j++){
+                if(i-nums[j]>=0)
+               dp[i]+= dp[i-nums[j]];
+            }
+            // dp[i]=ans;
+        }
+        return dp[dp.size()-1];
+    }
+};
