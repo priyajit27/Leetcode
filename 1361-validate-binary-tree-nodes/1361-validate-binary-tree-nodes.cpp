4005a14e520f8abb31252c7bed37514410c80c2c
Priyajit Paul <97836046+priyajit27@users.noreply.github.com>
2023-10-18 00:59:36 +0530

    Time: 34 ms (77.20%), Space: 32.8 MB (73.25%) - LeetHub

---
+++ 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) {
+        vector<int>indegree(n,0);
+        for(int i=0;i<n;i++){
+           if(leftChild[i]!=-1) 
+               indegree[leftChild[i]]++;
+               if(rightChild[i]!=-1) 
+               indegree[rightChild[i]]++;
+        }
+        
+        int root=-1;
+          for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                if(root==-1)
+                    root=i;
+                else 
+                    return false;
+            }
+        }
+        
+         if(root==-1)
+             return false;
+        
+        queue<int>q;
+        vector<bool>visited(n,false);
+        
+        q.push(root);
+        // visited[root]=true;
+        
+        while(!q.empty()){
+          int node=q.front();
+            q.pop();
+            if(visited[node])
+               return false;
+           
+             visited[node]=true;
+            if(leftChild[node]!=-1)
+            q.push(leftChild[node]);
+            
+            if(rightChild[node]!=-1)
+            q.push(rightChild[node]);
+        }
+        
+        return accumulate(visited.begin(),visited.end(),0)==n;
+    }
+};
