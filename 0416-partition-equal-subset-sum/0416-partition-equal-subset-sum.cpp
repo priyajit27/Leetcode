40992381ae8c28043155906f5373e551be39f5f8
Priyajit Paul <97836046+priyajit27@users.noreply.github.com>
2023-11-04 17:51:48 +0530

    Time: 566 ms (10.96%), Space: 13.4 MB (70.18%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
-    bool sum1(int target,vector<int>& nums,int i,vector<vector<int>>& dp){
-        if(target==0)return true;
-        if(i==0)return target==nums[0];
-        if(dp[i][target]!=-1)return dp[i][target];
-        bool nottake=sum1(target,nums,i-1,dp);
-        bool take = false;
-        if(target>=nums[i])
-            take=sum1(target-nums[i],nums,i-1,dp);
-        return dp[i][target]= take || nottake;
-    }
     bool canPartition(vector<int>& nums) {
-        int n=nums.size();
+          int n=nums.size();
+        // if(n==1)return false;
         int sum=0;
         for(auto i:nums)
             sum+=i;
         if(sum%2)return false;
-        
-         // vector<vector<int>>dp(n,vector<int>(sum+1,-1));
-         // return sum1(sum/2,nums,n-1,dp);
-         vector<vector<bool>>dp(n,vector<bool>(sum+1,false));
+          vector<vector<bool>>dp(n,vector<bool>(sum/2+1,false));
         for(int i=0;i<n;i++)dp[i][0]=true;
+        if(sum/2>=nums[0])
         dp[0][nums[0]]=true;
         for(int i=1;i<n;i++){
         for(int target=1;target<=sum/2;target++){
