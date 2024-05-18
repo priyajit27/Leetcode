4cf7cef852275a4b00f8aa03fe35122e1042d11f
Priyajit Paul <97836046+priyajit27@users.noreply.github.com>
2024-05-18 12:34:36 +0530

    Time: 240 ms (68.13%), Space: 131.6 MB (15.76%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+         if(n<=2)
+        return 0;
+        vector<int>a(n,0);
+         for(int i=2;i<a.size();i++){
+              a[i]=i;
+         }
+         a[0]=0;
+         a[1]=0;
+         for(int i=2;i<n;i++){
+             if(a[i]!=0){
+                 for(int j=i+i;j<n;j+=i){
+                     a[j]=0;
+                 }
+             }
+         }
+         int count=0;
+         for(auto i:a){
+             if(i!=0)
+             count++;
+         }
+        return count;
+    }
+};
