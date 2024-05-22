d4d0da4e70c188c140225dda6da2acb0d1c9852f
Priyajit Paul <97836046+priyajit27@users.noreply.github.com>
2024-05-23 00:26:48 +0530

    Time: 18 ms (54.18%), Space: 26.7 MB (70.95%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -18,8 +18,9 @@ public:
         for(int i=0;i<nums.size();i++){
             if(count==0){
                 element=nums[i];
+                count++;
             }
-            if(element==nums[i]){
+            else if(element==nums[i]){
                 count++;
             }
             else
