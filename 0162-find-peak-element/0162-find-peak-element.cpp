32c7af95fcece970fc0c985caa374319f19cd403
Priyajit Paul <97836046+priyajit27@users.noreply.github.com>
2024-05-25 11:33:52 +0530

    Time: 0 ms (100.00%), Space: 11.3 MB (50.70%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -9,6 +9,10 @@ public:
      int mid;
       if(n==1)
           return 0;
+        // if(arr[0]>arr[1])
+        //     return 0;
+        //    if(arr[n-1]>arr[n-2])
+        //     return n-1;
 	 while(low<=high){
           mid=(high+low)/2;
         
@@ -33,6 +37,6 @@ public:
              low=mid+1;
              
 	 }
-        return low; 
+        return -1; 
     }
 };
