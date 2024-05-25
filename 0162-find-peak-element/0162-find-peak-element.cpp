9076f4743288d52a756de4bde0ed4f792bd7648e
Priyajit Paul <97836046+priyajit27@users.noreply.github.com>
2024-05-25 11:36:17 +0530

    Time: 0 ms (100.00%), Space: 11.4 MB (10.91%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -9,25 +9,14 @@ public:
      int mid;
       if(n==1)
           return 0;
-        // if(arr[0]>arr[1])
-        //     return 0;
-        //    if(arr[n-1]>arr[n-2])
-        //     return n-1;
+        if(arr[0]>arr[1])
+            return 0;
+           if(arr[n-1]>arr[n-2])
+            return n-1;
+        low=1;high=n-2;
 	 while(low<=high){
           mid=(high+low)/2;
         
-         
-         if(mid==0){
-             if(arr[0]>arr[1])
-                 return 0;
-             return 1;
-         }
-         if(mid==n-1){
-             if(arr[n-1]>arr[n-2])
-                 return n-1;
-             return n-2;
-         }
-      
          if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1])
              return mid;
          
