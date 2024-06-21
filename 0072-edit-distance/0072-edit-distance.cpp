e59511d455991a86124e86fecd086c654e61c584
Priyajit Paul <97836046+priyajit27@users.noreply.github.com>
2024-06-22 01:43:41 +0530

    Time: 13 ms (29.31%), Space: 12 MB (34.78%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int rec(string s1,string s2,int i,int j, vector<vector<int>>& dp){
         if(i<0)
-            return j;
+            return j+1;
         if(j<0)
-            return i;
+            return i+1;
         if(dp[i][j]!=-1)
             return dp[i][j];
         if(s1[i]==s2[j])
@@ -14,44 +14,44 @@ public:
     int minDistance(string word1, string word2) {
         int n=word1.length();
         int m=word2.length();
-         // vector<vector<int>>dp(n,vector<int>(m,-1));      
-         // return rec(word1,word2,n-1,m-1,dp);
+//          vector<vector<int>>dp(n,vector<int>(m,-1));      
+//          return rec(word1,word2,n-1,m-1,dp);
         
         
         // tabulation
-        //   vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        // for(int j=0;j<=m;j++){
-        //     dp[0][j]=j;
-        // }
-        //   for(int i=0;i<=n;i++){
-        //     dp[i][0]=i;
-        // }
-        // for(int i=1;i<=n;i++){
-        //     for(int j=1;j<=m;j++){
-        //         if(word1[i-1]==word2[j-1])
-        //             dp[i][j]=dp[i-1][j-1];
-        //         else
-        //            dp[i][j] =1+min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
-        //     }
-        // }
-        // return dp[n][m];
-        
-        
-         vector<int>curr(m+1,0);       
-         vector<int>prev(m+1,0);
+          vector<vector<int>>dp(n+1,vector<int>(m+1,0));
         for(int j=0;j<=m;j++){
-            prev[j]=j;
+            dp[0][j]=j;
+        }
+          for(int i=0;i<=n;i++){
+            dp[i][0]=i;
         }
         for(int i=1;i<=n;i++){
-            curr[0]=i;
             for(int j=1;j<=m;j++){
                 if(word1[i-1]==word2[j-1])
-                    curr[j]=prev[j-1];
+                    dp[i][j]=dp[i-1][j-1];
                 else
-                   curr[j] =1+min(prev[j-1],min(prev[j],curr[j-1]));
+                   dp[i][j] =1+min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
             }
-            prev=curr;
         }
-        return prev[m];
+        return dp[n][m];
+        
+        
+//          vector<int>curr(m+1,0);       
+//          vector<int>prev(m+1,0);
+//         for(int j=0;j<=m;j++){
+//             prev[j]=j;
+//         }
+//         for(int i=1;i<=n;i++){
+//             curr[0]=i;
+//             for(int j=1;j<=m;j++){
+//                 if(word1[i-1]==word2[j-1])
+//                     curr[j]=prev[j-1];
+//                 else
+//                    curr[j] =1+min(prev[j-1],min(prev[j],curr[j-1]));
+//             }
+//             prev=curr;
+//         }
+//         return prev[m];
     }
 };
