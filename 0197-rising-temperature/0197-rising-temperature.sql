8cb55bf5b4e62b35c52ed950432a607a2aaf1bb4
Priyajit Paul <97836046+priyajit27@users.noreply.github.com>
2024-06-24 16:59:36 +0530

    Time: 420 ms (88.79%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -3,6 +3,6 @@ SELECT DISTINCT a.id
 FROM
 Weather as a
 INNER JOIN Weather as b
-WHERE  DATEDIFF(a.recordDate,b.recordDate)=1
+ON  DATEDIFF(a.recordDate,b.recordDate)=1
 AND a.temperature >b.temperature 
 ORDER BY id ASC;
