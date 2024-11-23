ab88c53e152b45b2c609eb193cacff78af5bdf4f
Priyajit Paul <97836046+priyajit27@users.noreply.github.com>
2024-11-23 21:09:27 +0530

    Time: 0 ms (100.00%), Space: 20.8 MB (39.97%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -6,9 +6,11 @@ public:
         // int xr2=0;
         for(int i=0;i<n;i++){
             xr1^=nums[i];
-            xr1^=i;
+            xr1^=i+1;
         }
-         xr1^=n;   
+         // xr1^=n;   
         return xr1;
+        
+    
     }
 };
